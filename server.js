3635765635cd08d84cf8a349064e2082235bece9
Slobodan <boba_dj1987@hotmail.com>
2014-09-10 11:50:50 +0200

    Test configuration
    Test configuration

--- server.js
+++ server.js
@@ -3,10 +3,30 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-    res.send('hello world');
+app.get('/test', function(req, res){
+    res.send( getTweet() );
 });
 
 app.listen(3000, '127.0.0.1', function(){
   console.log('node server started');
 });
+
+var util = require('util'),
+    twitter = require('twitter');
+
+var twit = new twitter({
+    consumer_key: 'dDDYuvE5eyBsAD0ouOK1C7MtQ',
+    consumer_secret: 'NrNzM1Ej7qfdfH0YDpdY6mwU0oJkIddSH3gqNJbDqs93KP5OXZ',
+    access_token_key: '1129618712-GTaSK0N8UtSDZJTA0Kklgpn9wlZk1adL3vokAnp',
+    access_token_secret: 'FpzsoOQaHHtSltwlnpFL8urLaxamUjM3nZZEHv4B3hv2C'
+});
+
+var utils;
+
+var getTweet = function(){ 
+	twit.get('/statuses/show/27593302936.json', {include_entities:true}, function(data) {
+    	utils = data;
+	});
+
+	return utils;
+};
