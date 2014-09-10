1245061936aa43cc665cbe439a433a2118aa2bba
Slobodan <boba_dj1987@hotmail.com>
2014-09-10 13:49:14 +0200

    Getting last 15 tweets setting
    Getting last 15 tweets setting

--- public/test.js
+++ public/test.js
@@ -5,8 +5,11 @@ $(function(){
   		dataType: "json",
   		success: function(data){
   			console.log(data);
-  			
-  			$('body').append( '<h1>Server response</h1> <p>' + data.user.description + ' by: ' + data.user.name + '</p>' )
+  			$('body').append( '<h1>Last 15 tweets containing #node or "nodeJs":</h1>');
+
+  			$.each(data.statuses, function(){
+  				$('body').append( '<p>' + this.text + '</p>');
+  			});
   		}
 	});
 }());
