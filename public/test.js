b3ecf05d34f3d4a172149233cfa26bd4d837ba5f
Slobodan <boba_dj1987@hotmail.com>
2014-09-10 11:50:42 +0200

    Test configuration
    Test configuration

--- public/test.js
+++ public/test.js
@@ -1,1 +1,13 @@
-console.log('hello test.js');
+$(function(){
+	$.ajax({
+		url: "http:/test",
+  		type: "GET",
+  		dataType: "json",
+  		success: function(data){
+  			console.log(data);
+  			
+  			$('body').append( '<h1>Server response</h1> <p>' + data.user.description + ' by: ' + data.user.name + '</p>' )
+  		}
+	});
+}());
+
