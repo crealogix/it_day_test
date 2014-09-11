e1a84002e3be5b6bba952d7adc77c19cdd7d42d3
Slobodan <boba_dj1987@hotmail.com>
2014-09-11 16:37:29 +0200

    Separating backend functions, and server settings.

---
+++ app/backendApp.js
@@ -0,0 +1,20 @@
+var twitter = require('twitter'),
+	 Q = require('q');
+
+var twit = new twitter({
+    consumer_key: 'dDDYuvE5eyBsAD0ouOK1C7MtQ',
+    consumer_secret: 'NrNzM1Ej7qfdfH0YDpdY6mwU0oJkIddSH3gqNJbDqs93KP5OXZ',
+    access_token_key: '1129618712-GTaSK0N8UtSDZJTA0Kklgpn9wlZk1adL3vokAnp',
+    access_token_secret: 'FpzsoOQaHHtSltwlnpFL8urLaxamUjM3nZZEHv4B3hv2C'
+});
+
+exports.getTweet = function(){
+    var deferred = Q.defer();
+
+    twit.search('nodejs OR #node', {include_entities:true, count: 15}, function(data) {
+        deferred.resolve( data );
+    });
+
+    return deferred.promise;
+};
+
--- server.js
+++ server.js
@@ -1,34 +1,15 @@
 var express = require('express'),
     app = express(),
-    Q = require('q');
+    backend = require('./app/backendApp');
 
 app.use(express.static(__dirname + '/public'));
-
-app.get('/test', function(req, res){
-    getTweet().then(function(val){
-        res.send( val );
-    })
-});
 
 app.listen(3000, '127.0.0.1', function(){
   console.log('node server started');
 });
 
-var twitter = require('twitter');
-
-var twit = new twitter({
-    consumer_key: 'dDDYuvE5eyBsAD0ouOK1C7MtQ',
-    consumer_secret: 'NrNzM1Ej7qfdfH0YDpdY6mwU0oJkIddSH3gqNJbDqs93KP5OXZ',
-    access_token_key: '1129618712-GTaSK0N8UtSDZJTA0Kklgpn9wlZk1adL3vokAnp',
-    access_token_secret: 'FpzsoOQaHHtSltwlnpFL8urLaxamUjM3nZZEHv4B3hv2C'
+app.get('/test', function(req, res){
+    backend.getTweet().then(function(val){
+        res.send( val );
+    })
 });
-
-function getTweet() {
-    var deferred = Q.defer();
-
-    twit.search('nodejs OR #node', {include_entities:true, count: 15}, function(data) {
-        deferred.resolve( data );
-    });
-
-    return deferred.promise;
-};
