abcd13da9548dc76379bfd9d4b7f6f14b1d616ec
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2014-10-12 00:33:28 +0200

    showing and hidding buy and sell button

--- public/js/widgetObserver.js
+++ public/js/widgetObserver.js
@@ -4,15 +4,21 @@ CLX.prototype.TweetScanner = function(){
 
 	self = this;
 	criticalWords.good = ['brand new', 'excited to see', 'launched ', 'future', 'deal to close', 'goes up', 'makes it easier', 'happy', 'started'];
+	criticalWords.bad = ['milestone', 'stay away'];
 
 	//parse string
 	self.scanTweet = function(string){
-		for(var prop in criticalWords){
-			for(i=0;i<criticalWords[prop].length;i++){
-				//if some critical work found, increse progress bar
-				if( string.indexOf(criticalWords[prop][i]) > -1 ){
-					result = result - 10;
-				}
+		for(i=0;i<criticalWords.good.length;i++){
+			//if some goor critical work found, increse progress bar
+			if( string.indexOf(criticalWords.good[i]) > -1 ){
+				result = result - 10;
+			}
+		}
+
+		for(i=0;i<criticalWords.bad.length;i++){
+			//if some goor critical work found, increse progress bar
+			if( string.indexOf(criticalWords.bad[i]) > -1 ){
+				result = result + 10;
 			}
 		}
 	};
@@ -24,6 +30,8 @@ CLX.prototype.TweetScanner = function(){
 
 		if( result < 0 ){
 			result = 0;
+		}else if( result >= 100 ){
+			result = 90;
 		}
 
 		return result;
@@ -47,7 +55,7 @@ CLX.AddToList = function(addTo, addFrom){
 
 	//get tweets
 	self.getObserver = function(stock, amount){
-		var stockNameParsed = stock.toLowerCase().split(' ').join('_');
+		var stockNameParsed = stock.toLowerCase().split(' ').join('_'), action;
 
 		//count stocks
 		stockCounter++;
@@ -80,9 +88,17 @@ CLX.AddToList = function(addTo, addFrom){
 				barWidth = setter;
 			}
 
+			if( barWidth < 50 ){
+				action = "buy"
+			}else if( barWidth > 50 ){
+				action = "sell"
+			}else{
+				action = "hold"
+			}
+
 			stockCounter--;
 
-			self.appendToList(stock, amount);
+			self.appendToList(stock, amount, action);
 
 			if(stockCounter === 0){
 				console.log('Load done!');
@@ -132,10 +148,10 @@ CLX.AddToList = function(addTo, addFrom){
 		});
 	};
 
-	self.appendToList = function(stockName, amount){
+	self.appendToList = function(stockName, amount, action){
 		var stockNameParsed = stockName.split(' ').join('_'),
 			amount = amount || 0, 
-			tableRowTemplate = '<ul id="stock_'+ stockNameParsed +'" class="table-content clearfix"><li class="width-15 capital-text">'+ stockName +'</li><li class="amount width-7 text-center">' + amount + '</li><li class="text-right width-12">--</li><li class="text-right width-12">--</li><li class="text-right width-7"><p class="font-12-success padding-5-0"><span class="font-bold"> -- %</span></p></li><li class="width-25"><div class="indicator"><span class="indicator-positive">Positive</span><span class="indicator-negative">Negative</span><span class="mask" style="width:'+ barWidth +'%"></span><span class="bar"></span></div></li><li> <input data-trade="Buy" type="button" value="Buy" data-stock="' + stockName + '" class="btn btn-info"> <input data-trade="Sell" type="button" value="Sell" data-stock="' + stockName + '" class="btn btn-success"> <input data-trade="stop" type="button" data-stock="' + stockName + '" value="Tweets" class="btn btn-primary getTweets" data-toggle="modal" data-target=".bs-example-modal-lg.twet-modal"> </li></ul>';
+			tableRowTemplate = '<ul id="stock_'+ stockNameParsed +'" class="table-content clearfix ' + action +'"><li class="width-15 capital-text">'+ stockName +'</li><li class="amount width-7 text-center">' + amount + '</li><li class="text-right width-12">--</li><li class="text-right width-12">--</li><li class="text-right width-7"><p class="font-12-success padding-5-0"><span class="font-bold"> -- %</span></p></li><li class="width-25"><div class="indicator"><span class="indicator-positive">Positive</span><span class="indicator-negative">Negative</span><span class="mask" style="width:'+ barWidth +'%"></span><span class="bar"></span></div></li><li> <input data-trade="Buy" type="button" value="Buy" data-stock="' + stockName + '" class="btn btn-info"> <input data-trade="Sell" type="button" value="Sell" data-stock="' + stockName + '" class="btn btn-success"> <input data-trade="stop" type="button" data-stock="' + stockName + '" value="Tweets" class="btn btn-primary getTweets" data-toggle="modal" data-target=".bs-example-modal-lg.twet-modal"> </li></ul>';
 		$( '.preloader' ).before( $(tableRowTemplate) );
 	};
 
@@ -143,6 +159,7 @@ CLX.AddToList = function(addTo, addFrom){
 	function showTweets(){
 		$( getListHolder(addTo) ).on('click', '.getTweets', function() {
 		    var stockName = $(this).data().stock,
+		   		stockNameParsed = stockName.split(' ').join('_').toLowerCase(),
 		    	modalList = $('.modal-content>.tweet-list'),
 		    	modalHead = $('.modal-content>.tweet-topic'),
 		    	tweetText,
@@ -153,9 +170,9 @@ CLX.AddToList = function(addTo, addFrom){
 
 		    modalHead.append( stockName + ' tweets:');
 
-		    for(i=0;i<tweets[stockName].statuses.length;i++){
-		    	tweetText = tweets[stockName].statuses[i].text;
-		    	userName = tweets[stockName].statuses[i].user.name;
+		    for(i=0;i<tweets[stockNameParsed].statuses.length;i++){
+		    	tweetText = tweets[stockNameParsed].statuses[i].text;
+		    	userName = tweets[stockNameParsed].statuses[i].user.name;
 		    	modalList.append( 'By ' + userName + ':  ' + formatText(tweetText, stockName.toLowerCase() ) + '<br/>');
 		    }
 		});
@@ -204,7 +221,6 @@ var addTo = CLX.AddToList('stock-table-holder');
 addTo.getObserver('IBM', 25000);
 addTo.getObserver('Societe Generale', 10000);
 addTo.getObserver('General Electrics', 1000);
-addTo.getObserver('YAHOO', 1000000);
 addTo.getObserver('Microsoft', 30000);
 addTo.getObserver('Google', 5000);
 
